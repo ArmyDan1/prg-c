122f07287815879a14408049a1889cef5d896b86
ArmyDan1 <47758862+ArmyDan1@users.noreply.github.com>
2021-01-04 10:36:50 +0100

    Add files via upload

---
+++ du prg 444.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void ahoj(int n)
+{
+    for (int i = 0; i<n; i++)
+    {
+        printf("ahoj kámo\n");
+    }
+}
+
+int main()
+{
+    ahoj(9);
+    return 0;
+}
