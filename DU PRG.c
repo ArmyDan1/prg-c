902b3a841bbeeb6a5e293813dcb63a33d9185d3a
ArmyDan1 <47758862+ArmyDan1@users.noreply.github.com>
2021-01-04 10:15:10 +0100

    Add files via upload

---
+++ DU PRG.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+ 
+ 
+ printf("                                     SEZNAM ŽÁKŮ\n             IT:\n");
+   
+   int i;                    
+   
+   for(i=1; i<=20; i++){
+       printf("%i\n", i);
+   }
+   
+    printf("                                    SEZNAM ŽÁKŮ\n             ET:\n");
+   
+   int u;
+   for(u=1; u<=9; u++){
+       printf("%i\n", u);
+   }
+   
+   return 0;
+}
